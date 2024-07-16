1bf4255ecd8e966169a2bbe23a952dcf842ac085
event_website <sainiumang0022@gmail.com>
2024-07-16 13:18:43 +0530

    implement stripe

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,32 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         domains: ['utfs.io'],
+//         remotePatterns: [
+//             {
+//                 protocol: 'https',
+//                 hostname: 'utfs.io',
+//                 port: '',
+//             }
+//         ]
+//     }
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['utfs.io'],
         remotePatterns: [
             {
                 protocol: 'https',
                 hostname: 'utfs.io',
                 port: '',
+                pathname: '/**', // Adjust this if you need more specific paths
             }
         ]
     }
 };
 
 export default nextConfig;
+
