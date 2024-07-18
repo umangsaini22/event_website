844fccda784eed74280e763b7b4e916330e57edd
event_website <sainiumang0022@gmail.com>
2024-07-18 23:07:17 +0530

    Update source code

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -184,7 +184,7 @@ export const checkoutOrder = async (order: CheckoutOrderParams) => {
       buyerId: order.buyerId
     },
     mode: 'payment',
-    success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}}/profile`,
+    success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
     cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
     });
 
